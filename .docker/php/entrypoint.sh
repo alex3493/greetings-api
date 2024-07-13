4992e2dfaa7611b88690728c088de946ca30f540
Alex <alex.polonski@gmail.com>
2024-07-13 19:01:06 +0200

    Fix entrypoint.sh line separator.

--- .docker/php/entrypoint.sh
+++ .docker/php/entrypoint.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-
-composer install -n
-php bin/console lexik:jwt:generate-keypair --skip-if-exists
-php bin/console doctrine:migrations:migrate --no-interaction
-
-exec "$@"
+composer install -n && php bin/console lexik:jwt:generate-keypair --skip-if-exists && php bin/console doctrine:migrations:migrate --no-interaction && exec "$@"
