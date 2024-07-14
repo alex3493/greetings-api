63b7330269c406b3ad98280eb8702289db6f0a59
Alex <alex.polonski@gmail.com>
2024-07-14 14:30:51 +0200

    entrypoint.sh - do not display errors when migrating test database.

--- .docker/php/entrypoint.sh
+++ .docker/php/entrypoint.sh
@@ -3,5 +3,5 @@ composer install -n
 php bin/console lexik:jwt:generate-keypair --skip-if-exists
 php bin/console doctrine:migrations:migrate --no-interaction
 php bin/console doctrine:database:create --if-not-exists --no-interaction --env=test
-php bin/console doctrine:migrations:migrate --no-interaction --env=test
+php bin/console doctrine:migrations:migrate --no-interaction --quiet --env=test
 exec "$@"
