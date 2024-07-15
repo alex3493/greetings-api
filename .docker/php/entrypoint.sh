8bde5c449bf61c68858547d5de0b0ced2c82d72a
Alex <alex.polonski@gmail.com>
2024-07-15 19:17:32 +0200

    Import changes from main.

--- .docker/php/entrypoint.sh
+++ .docker/php/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-composer install -n
+vendor_dir="/var/www/symfony/vendor"
+if [ ! "$(ls -A $vendor_dir)" ]; then
+  composer install -n
+fi
 php bin/console lexik:jwt:generate-keypair --skip-if-exists
 php bin/console doctrine:migrations:migrate --no-interaction
 php bin/console doctrine:database:create --if-not-exists --no-interaction --env=test
 php bin/console doctrine:schema:update --no-interaction --force --complete --env=test
 exec "$@"
-
